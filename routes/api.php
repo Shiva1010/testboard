9c0f4b55abd8be75101e8d6f5f8fa119d6da8534
Shiva <shivaxsin@gmail.com>
2020-03-19 10:38:22 +0800

    feat: Route/api 設定使用者、留言、評論、回覆對應 Controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 使用者 SuserController
+// 註冊
+Route::post('/register','SuserController@store');
+Route::post('/login','SuserController@login');
+
+
+Route::group(['middleware' => ['auth:suser']], function() {
+// 留言版  BoardController
+// 寫留言
+    Route::post('/board', 'BoardController@store');
+
+// 查看留言
+    Route::get('/board/all', 'BoardController@index');
+
+// 按讚跟收回讚
+    Route::post('/board/good', 'BoardController@good');
+
+// 讚數
+    Route::get('/board/goodcount', 'BoardController@goodcount');
+
+
+// 查讚者
+    Route::post('/board/goodwho', 'BoardController@goodwho');
+
+
+// 評論留言 MsgController
+// 寫評論
+    Route::post('/msg', 'MsgController@msg');
+
+
+// 回覆評論 ReMsgController
+// 寫回覆
+    Route::post('/remsg', 'ReMsgController@remsg');
+});
